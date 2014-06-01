a26c376cbd68933132d407ff1bc731039dc3e0b1
Daniel Egger <daniel.egger@gmail.com>
2014-06-01 12:08:50 +0200

    ch01 exercises

--- ch01/ex01.rkt
+++ ch01/ex01.rkt
@@ -112,3 +112,63 @@
         result
         (iter (next a) (+ result (term a)))))
   (iter a 0))
+
+; 1.31
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (term a)
+         (product term (next a) next b))))
+
+(define (product-iter term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (* result (term a)))))
+  (iter a 1))
+
+(define (factorial n)
+  (product identity 1 inc n))
+
+(define (wallis-pi-product n)
+  (define (term x)
+    (if (even? x)
+        (/ (+ x 2.0) (+ x 1.0))
+        (/ (+ x 1.0) (+ x 2.0))))
+  (acc-product term 1 inc n))
+
+; 1.32
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a)
+                (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate-iter combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+(define (acc-product term a next b)
+  (accumulate-iter * 1 term a next b))
+
+(define (acc-sum term a next b)
+  (accumulate-iter + 0 term a next b))
+
+; ex 1.33
+(define (filtered-accumulate combiner null-value term a next b filter)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (if (filter a)
+            (iter (next a) (combiner result (term a)))
+            (iter (next a) result))))
+  (iter a null-value))
+
+(define (filtered-sum term a next b filter)
+  (filtered-accumulate + 0 term a next b filter))
+
+(define (sum-prime-squares a b)
+  (filtered-sum sqr a inc b exact-prime?))
---
+++ ch01/notes.md
@@ -0,0 +1,4 @@
+# Chapter 1
+
+## 1.3.2
+* A let expression is simply syntactic sugar for the underlying lambda application.
