cf0f6231493d170452e5465966d55e2d168951e5
Daniel Egger <daniel.egger@gmail.com>
2014-06-11 17:18:21 +0200

    added files for ch02

---
+++ ch02.rkt
@@ -0,0 +1,43 @@
+#lang racket
+                                        ;
+;2.1
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y)))) (define (div-rat x y)
+                                        (make-rat (* (numer x) (denom y))
+                                                  (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (let ((numer (/ n g))
+          (denom (/ d g)))
+      (if (and (negative? denom) (positive? numer))
+          (cons (- numer) (- denom))
+          (cons numer denom)))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x))
+  (newline))
