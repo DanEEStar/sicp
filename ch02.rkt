05c51ab9723b1e9b773a7731d6e1fbf3aef0acae
Daniel Egger <daniel.egger@gmail.com>
2014-06-15 01:42:18 +0200

    up to ex 2.6

--- ch02.rkt
+++ ch02.rkt
@@ -1,6 +1,27 @@
 #lang racket
-                                        ;
 ;2.1
+
+;(define my-cons cons)
+;(define my-car car)
+;(define my-cdr cdr)
+
+;(define (my-cons x y)
+;  (define (dispatch m)
+;    (cond ((= m 0) x)
+;          ((= m 1) y)
+;          (else (error "Argument not 0 or 1: CONS" m))))
+;  dispatch)
+;(define (my-car z) (z 0))
+;(define (my-cdr z) (z 1))
+
+; ex 2.4
+(define (my-cons x y)
+  (lambda (m) (m x y)))
+(define (my-car z)
+  (z (lambda (p q) p)))
+(define (my-cdr z)
+  (z (lambda (p q) q)))
+
 (define (add-rat x y)
   (make-rat (+ (* (numer x) (denom y))
                (* (numer y) (denom x)))
@@ -20,21 +41,16 @@
   (= (* (numer x) (denom y))
      (* (numer y) (denom x))))
 
-(define (gcd a b)
-  (if (= b 0)
-      a
-      (gcd b (remainder a b))))
-
 (define (make-rat n d)
   (let ((g (gcd n d)))
     (let ((numer (/ n g))
           (denom (/ d g)))
       (if (and (negative? denom) (positive? numer))
-          (cons (- numer) (- denom))
-          (cons numer denom)))))
+          (my-cons (- numer) (- denom))
+          (my-cons numer denom)))))
 
-(define (numer x) (car x))
-(define (denom x) (cdr x))
+(define (numer x) (my-car x))
+(define (denom x) (my-cdr x))
 
 (define (print-rat x)
   (display (numer x))
--- ex02.rkt
+++ ex02.rkt
@@ -27,3 +27,40 @@
                        (x-point (end-segment s)))
               (average (y-point (start-segment s))
                        (y-point (end-segment s)))))
+
+; ex 2.5
+(define (factor-out n factor)
+  (if (> (remainder n factor) 0)
+      n
+      (factor-out (/ n factor) factor)))
+
+(define (nth-root n base)
+  (define (iter n res)
+    (if (= n base)
+        res
+        (iter (/ n base) (+ res 1))))
+  (iter n 1))
+
+(define (integer-cons a b)
+  (* (expt 2 a) (expt 3 b)))
+
+(define (integer-car x)
+  (nth-root (factor-out x 3) 2))
+
+(define (integer-cdr x)
+  (nth-root (factor-out x 2) 3))
+
+; ex 2.6
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x)))))
+(define (zero f) (lambda (x) x))
+(define (one f) (lambda (x) (f x)))
+(define (two f) (lambda (x) (f (f x))))
+(define (three f) (lambda (x) (f (f (f x)))))
+(define (plus a b)
+  (lambda (f) (lambda (x) ((a f) ((b f) x)))))
+(define (mult a b)
+  (lambda (f) (a (b f))))
+
+(define (inc n)
+  (+ n 1))
