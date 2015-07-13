00cef29f16bf5961bd76ce922b9becaa2a548063
Daniel Egger <daniel.egger@gmail.com>
2015-07-13 17:07:50 +0200

    trees

--- ch02_3.rkt
+++ ch02_3.rkt
@@ -84,6 +84,8 @@
 (define (base e) (cadr e))
 (define (exponent e) (caddr e))
 
+; 2.3.3
+; sets as unordered lists
 (define (element-of-set-uo? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
@@ -106,11 +108,12 @@
          (union-set-uo (cdr set1) set2))
         (else (cons (car set1) (union-set-uo (cdr set1) set2)))))
 
-(define (element-of-set-o? x set)
+; set as ordered lists
+(define (element-of-set-s? x set)
   (cond ((null? set) false)
         ((= x (car set)) true)
         ((< x (car set)) false)
-        (else (element-of-set-o? x (cdr set)))))
+        (else (element-of-set-s? x (cdr set)))))
 
 (define (intersection-set-s set1 set2)
   (if (or (null? set1) (null? set2)) '()
@@ -145,3 +148,76 @@
                   (cons x1 (union-set-s (cdr set1) set2)))
                  ((< x2 x1)
                   (cons x2 (union-set-s set1 (cdr set2)))))))))
+
+; sets as binary trees
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set-b? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set-b? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set-b? x (right-branch set)))))
+
+(define (adjoin-set-b x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set-b x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set) (left-branch set)
+                    (adjoin-set-b x (right-branch set))))))
+
+; ex 2.63
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1
+                     (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list (left-branch tree)
+                      (cons (entry tree)
+                            (copy-to-list
+                             (right-branch tree)
+                             result-list)))))
+  (copy-to-list tree '()))
+
+(define testtree1
+  (make-tree 7
+             (make-tree 3
+                        (make-tree 1 '() '())
+                        (make-tree 5 '() '()))
+             (make-tree 9
+                        '()
+                        (make-tree 11 '() '()))))
+
+(define testtree2
+  (make-tree 3
+             (make-tree 1 '() '())
+             (make-tree 7
+                        (make-tree 5 '() '())
+                        (make-tree 9
+                                   '()
+                                   (make-tree 11 '() '())))))
+
+(define testtree3
+  (make-tree 5
+             (make-tree 3
+                        (make-tree 1 '() '())
+                        '())
+             (make-tree 9
+                        (make-tree 7 '() '())
+                        (make-tree 11 '() '()))))
